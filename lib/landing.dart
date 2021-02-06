705500c8808454933baf4efe06b40c9c4c441693
jessehu312 <jessehu312@gmail.com>
2021-02-06 14:24:49 -0800

    Refactor Landing

---
+++ lib/landing.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class Landing extends StatelessWidget {
+
+  void _login() {
+
+  }
+
+  void _register() {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Immunize',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: new Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding (
+            padding: new EdgeInsets.all(23),
+            child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: Rotator()),
+              ElevatedButton(
+                onPressed: _login,
+                child: Text('Login', style: TextStyle(fontSize: 20))
+              ),
+              TextButton(
+                onPressed: _register,
+                child: Text('Register', style: TextStyle(fontSize: 20))
+              )
+            ]
+          ),
+        )
+      )
+    );
+  }
+}
+
+class Rotator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+        options: CarouselOptions(),
+        items: [
+          Image(image: AssetImage('assets/images/rotate1.png')),
+          Image(image: AssetImage('assets/images/rotate2.png')),
+          Image(image: AssetImage('assets/images/rotate3.png')),
+        ].map((item) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: item);
+            },
+          );
+        }).toList(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
+
+import 'package:immunize/landing.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -43,44 +44,5 @@ class Loading extends StatelessWidget {
             color: Color.fromARGB(255, 45, 76, 198),
             child: Image(image: AssetImage('assets/images/immunize.png')),
           );
-  }
-}
-
-class Landing extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Immunize',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: CarouselSlider(
-        options: CarouselOptions(height: 400.0),
-        items: [
-          Image(image: AssetImage('assets/images/rotate1.png')),
-          Image(image: AssetImage('assets/images/rotate2.png')),
-          Image(image: AssetImage('assets/images/rotate3.png')),
-        ].map((item) {
-          return Builder(
-            builder: (BuildContext context) {
-              return Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: BoxDecoration(color: Colors.white),
-                  child: item);
-            },
-          );
-        }).toList(),
-      ),
-    );
   }
 }
