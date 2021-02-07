2b85b61425953579561e9250466f447fd6288e81
aryanvichare <aryanvichare10@gmail.com>
2021-02-06 17:47:04 -0800

    add hex color utility and constants file

---
+++ lib/utils/constants.dart
@@ -0,0 +1,26 @@
+// Name of our app
+import 'package:flutter/material.dart';
+import 'package:immunize/utils/hex_color.dart';
+
+final String APP_NAME = 'Immunize';
+
+// Color Palette
+final String kPrimaryColor = "2D4CC6";
+final String kSecondaryColor = "213258";
+
+// Margins & Padding
+const double kGutterSmall = 20;
+
+// Typography
+
+const kHeadingStyle = TextStyle(
+  color: Color.fromARGB(255, 45, 76, 198),
+  fontSize: 18.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kLinkStyle = TextStyle(
+  color: Color.fromARGB(255, 45, 76, 198),
+  fontSize: 14.0,
+  fontWeight: FontWeight.normal,
+);
